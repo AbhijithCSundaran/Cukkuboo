555e0a850fe8e6606748a598f4eda42f289db68f
Parvathy <parusmenon18@gmail.com>
2025-06-18 18:41:08 +0530

    signin

--- cukkuboo-website/src/app/pages/sign-in/sign-in.component.ts
+++ cukkuboo-website/src/app/pages/sign-in/sign-in.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [
+    MatSnackBarModule,ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.scss'
+  styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      rememberMe: [false]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+
+      if (username === 'admin' && password === 'admin123') {
+        this.snackBar.open('Login successful!', 'Close', {
+          duration: 3000,
+          panelClass: ['snackbar-success'], 
+          verticalPosition: 'top'
+        });
+      } else {
+        this.snackBar.open('Invalid username or password', 'Close', {
+          duration: 3000,
+          panelClass: ['snackbar-error'], 
+          verticalPosition: 'top'
+        });
+      }
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
 
 }

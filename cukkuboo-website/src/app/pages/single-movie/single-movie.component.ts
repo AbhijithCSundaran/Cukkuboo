39ba73c99fae2bedbb8392388c6168b4066788ce
Parvathy <parusmenon18@gmail.com>
2025-06-20 15:27:00 +0530

    singlemovie

--- cukkuboo-website/src/app/pages/single-movie/single-movie.component.ts
+++ cukkuboo-website/src/app/pages/single-movie/single-movie.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { MovieService } from '../../services/movie.service';
 import { CommonModule } from '@angular/common';
+import { environment } from '../../../environments/environment';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-single-movie',
@@ -13,26 +14,41 @@ import { CommonModule } from '@angular/common';
 })
 export class SingleMovieComponent implements OnInit {
   movie: any;
-  videoUrl: SafeResourceUrl | null = null;
+  video: string = '';
   thumbnail: string = '';
+  videoUrl = environment.apiUrl+'uploads/videos/';
+  imageUrl = environment.apiUrl+'uploads/images/';
 
   constructor(
     private route: ActivatedRoute,
+    private snackBar: MatSnackBar,
     private movieService: MovieService,
-    private sanitizer: DomSanitizer
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    if (id) {
-      this.movieService.getMovieById(id).subscribe({
-        next: (res) => {
-          const data = res.data;
-          this.movie = data;
-       
-        },
-        error: (err) => console.error('Movie fetch failed', err)
-      });
-    }
+    if (id)
+      this.getMovie(id)
+
+
+  }
+
+  getMovie(id: any) {
+    this.movieService.getMovieById(id).subscribe({
+      next: (res) => {
+        // console.log('Movie Data:', res);
+        if (res.data) {
+          this.movie = res.data;
+        }
+        else {
+          this.snackBar.open('Failed to load movie.', '', {
+            duration: 3000,
+            verticalPosition: 'top',
+            panelClass: ['snackbar-error']
+          });
+        }
+      },
+      error: (err) => console.error(err)
+    });
   }
 }

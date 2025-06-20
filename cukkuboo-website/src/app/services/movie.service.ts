53d646a0ef7ce1e27b037ab2dfe39a4edcffd172
Bala <balasaravanan.v@v4csolutions.com>
2025-06-20 10:08:08 +0530

    movies details service

--- cukkuboo-website/src/app/services/movie.service.ts
+++ cukkuboo-website/src/app/services/movie.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  private apiUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+get token() {
+    return localStorage.getItem('token') || sessionStorage.getItem('token');
+  }
+  get headers() {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.token}`
+    });
+  }
+  listMovies(pageIndex: number = 0, pageSize: number = 10, searchText: string = ''): Observable<any> {
+    return this.http.get(`${this.apiUrl}movie/moviedetails?pageIndex=${pageIndex}&pageSize=${pageSize}&searchText=${searchText}`, {
+      headers: this.headers
+    });
+  }
+
+
+
+
+
+
+
+
+
 }

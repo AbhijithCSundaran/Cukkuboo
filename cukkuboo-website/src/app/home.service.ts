a46676c359bb4873b9617351091047e54c9259e7
Parvathy <parusmenon18@gmail.com>
2025-06-23 16:08:54 +0530

    home

---
+++ cukkuboo-website/src/app/home.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  get token() {
+    return localStorage.getItem('token') || sessionStorage.getItem('token');
+  }
+
+  get headers() {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.token}`
+    });
+  }
+
+  
+  getHomeData(): Observable<any> {
+    return this.http.get(`${this.apiUrl}home`, { headers: this.headers
+    });
+  }
+
+
+}

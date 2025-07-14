29e3041a454736c580773021599b3511644a94fa
AbhijithCSundaran <abhijith.c.sundaran@gmail.com>
2025-07-14 15:36:14 +0530

    sign in

--- cukkuboo-website/src/environments/environment.prod.ts
+++ cukkuboo-website/src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
   imgUrl: '',
+  apiUrl: 'http://cukkuboo.com/api/', 
   // apiUrl: 'http://192.168.1.100/cukuboo/cukuboo-php/', 
   // apiUrl: 'https://v4cstaging.co.in/cukkuboo/cukkuboo-php/', 
-  apiUrl: 'https://v4cstaging.co.in/cukkuboo/qa/cukkuboo-php/',
+  // apiUrl: 'https://v4cstaging.co.in/cukkuboo/qa/cukkuboo-php/',
 
 };

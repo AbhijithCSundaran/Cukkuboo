4818fc17ebfe742ed327120b9ba615b597a10e8d
Parvathy <parusmenon18@gmail.com>
2025-07-15 18:56:35 +0530

    forget password bugs

--- Cukkuboo-Admin/src/environments/environment.ts
+++ Cukkuboo-Admin/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-   fileUrl: 'http://192.168.1.100/cukuboo/cukuboo-php/',
-  apiUrl: 'http://192.168.1.100/cukuboo/cukuboo-php/',
-  //   fileUrl: 'http://192.168.1.100/cukuboo/qa/cukuboo-php/',
-  // apiUrl: 'http://192.168.1.100/cukuboo/qa/cukuboo-php/',
+  //  fileUrl: 'http://192.168.1.100/cukuboo/cukuboo-php/',
+  // apiUrl: 'http://192.168.1.100/cukuboo/cukuboo-php/',
+    fileUrl: 'http://192.168.1.100/cukuboo/qa/cukuboo-php/',
+  apiUrl: 'http://192.168.1.100/cukuboo/qa/cukuboo-php/',
 };

49105a1a4b911ce88822fe8abc64fc119902aa90
DESKTOP-HQRGA2D\Bala <balasaravanan.v@v4csolutions.com>
2025-08-04 10:06:34 +0530

    confirm proceed in confirm plan

--- cukkuboo-website/src/environments/environment.ts
+++ cukkuboo-website/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  fileUrl: 'http://192.168.1.100/cukuboo/cukuboo-php/',
-  apiUrl: 'http://192.168.1.100/cukuboo/cukuboo-php/',
-  // imgUrl: 'https://v4cstaging.co.in/cukkuboo/qa/cukkuboo-php/',
-  // apiUrl: 'https://v4cstaging.co.in/cukkuboo/qa/cukkuboo-php/',
+  // fileUrl: 'http://192.168.1.100/cukuboo/cukuboo-php/',
+  // apiUrl: 'http://192.168.1.100/cukuboo/cukuboo-php/',
+  imgUrl: 'https://v4cstaging.co.in/cukkuboo/qa/cukkuboo-php/',
+  apiUrl: 'https://v4cstaging.co.in/cukkuboo/qa/cukkuboo-php/',
 };

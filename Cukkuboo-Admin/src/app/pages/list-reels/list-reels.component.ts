c447c573ab376354c83b27c9938fcbd6b567cd5d
Parvathy <parusmenon18@gmail.com>
2025-08-06 13:14:13 +0530

    formated count in K and M

--- Cukkuboo-Admin/src/app/pages/list-reels/list-reels.component.ts
+++ Cukkuboo-Admin/src/app/pages/list-reels/list-reels.component.ts
@@ -111,4 +111,13 @@ export class ListReelsComponent implements OnInit, AfterViewInit {
       panelClass: [panelClass]
     });
   }
+  formatCount(count: number): string {
+  if (count >= 1_000_000) {
+    return (count / 1_000_000).toFixed(1).replace(/\.0$/, '') + 'M';
+  } else if (count >= 1_000) {
+    return (count / 1_000).toFixed(1).replace(/\.0$/, '') + 'k';
+  }
+  return count.toString();
+}
+
 }
